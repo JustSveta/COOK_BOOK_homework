56621a96e76e6a86096fd9271bee90e9dcf65b7b
Svetlana Goryanaya <Goryanayasay@yandex.ru>
2023-11-18 00:26:56 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+# is it Task 1
+
+f = open('Cook_book.txt')
+print(type(f))
+data = f.read()
+print(data, type(data))
+f.close()
+
+
+
+
+# На выходе мы должны получить словарь с названием ингредиентов и его количества для блюда. Например, для такого вызова
+# get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2)
+
+
+# This is a sample Python script.
+
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
